2b0ef2232b10a4cdd1561c49fa300886be289956
Aaron Harpt <a169h161@ku.edu>
2021-06-19 12:36:00 -0500

    Change button id

--- src/Question.js
+++ src/Question.js
@@ -1,8 +1,11 @@
 import PossibleAnswer from "./PossibleAnswer"
+let i = 0;
 
 const Question = (props) => {
+
+    i++;
     return (
-    <div>
+    <div className="question-container">
         <div className="question">
             <h3 id="question-title">{props.title}</h3>
             { /* answers */ }
@@ -11,7 +14,7 @@ const Question = (props) => {
             <PossibleAnswer value={props.options[2]} />
             <PossibleAnswer value={props.options[3]} />
 
-            <button type="button" className="btn btn-secondary btn-sm submit-answer">Submit Answer</button>
+            <button id={"button-" + i} type="button" className="btn btn-secondary btn-sm submit-answer">Submit Answer</button>
         </div>
     </div>
     )
