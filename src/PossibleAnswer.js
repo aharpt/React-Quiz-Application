486edd91a9ce22456420e376a4264e6800e6a939
Aaron Harpt <a169h161@ku.edu>
2021-06-19 16:53:19 -0500

    Work on app UI

--- src/PossibleAnswer.js
+++ src/PossibleAnswer.js
@@ -7,7 +7,7 @@ const PossibleAnswer = (props) => {
     i++;
     return (
         <div className="form-check">
-            <input className="form-check-input" type="radio" name="exampleRadios" id={i} value="option1" />
+            <input className="form-check-input" type="radio" name="exampleRadios" id={i} value="" />
             <label className="form-check-label" htmlFor={i}>{props.value}</label>
         </div>
     )
--- src/Question.js
+++ src/Question.js
@@ -6,7 +6,7 @@ const Question = (props) => {
     i++;
     return (
     <div className="question-container">
-        <div className="question">
+        <div id={"question-" + i} className="question">
             <h3 id="question-title">{props.title}</h3>
             { /* answers */ }
             <PossibleAnswer value={props.options[0]} />
@@ -14,7 +14,11 @@ const Question = (props) => {
             <PossibleAnswer value={props.options[2]} />
             <PossibleAnswer value={props.options[3]} />
 
-            <button id={"button-" + i} type="button" className="btn btn-secondary btn-sm next">Next</button>
+            <button id={"button-" + i} type="button" className="btn btn-secondary next">Next&nbsp;
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-forward-fill" viewBox="0 0 16 16">
+                <path d="m9.77 12.11 4.012-2.953a.647.647 0 0 0 0-1.114L9.771 5.09a.644.644 0 0 0-.971.557V6.65H2v3.9h6.8v1.003c0 .505.545.808.97.557z"/>
+            </svg>
+            </button>
         </div>
     </div>
     )
